c8f3e495a70af5a9f597f8e69f05ae6b12fbe137
Faye Carpenter <carpenp@email.uscb.edu>
2026-02-15 03:21:43 -0500

    Began Psuedocode.

--- src/smallestLargestApp.java
+++ src/smallestLargestApp.java
@@ -3,5 +3,21 @@
  * @author patrickcarpenter
  */
 public class smallestLargestApp {
+    //
+    
+    /*Init. Phase*/
+    
+    // Import Random Number Class
+    // Initialize Array with 20 elements
+    // 
+    
+    /*Processing Phase*/
+    // For (int i = 0; i < 20; i++)
+        // Use Random Number Class to change each element to a random value
+    
+    
+    
+    
+    /*Termination Phase*/
     
 }
