a13f45dd17981b8d5e489d2d0713943ea108c373
Faye Carpenter <carpenp@email.uscb.edu>
2026-02-17 10:05:24 -0500

    Programing project finished.

--- src/smallestLargestApp.java
+++ src/smallestLargestApp.java
@@ -1,26 +1,48 @@
+import java.util.Random;
+
 /**
- *
- * @author patrickcarpenter
+ * Should have added this sooner:
+ * Credit to GeeksForGeeks for help with code!
+ * @author Faye Carpenter
  */
-public class smallestLargestApp 
+public class smallestLargestApp {
+    public static void main(String[] args){
+        
+        /*Init. Phase*/
+        // Initialize Array with 20 elements
+        int[] arr = new int[20];
+        // Minimum Value
+        int mini = 100;
+        // Maximum Value
+        int maxi = 0;
+        // Initialize Random Number Generator
+        Random rand = new Random();
+        
+        /*Processing Phase*/
+        for (int i = 0; i < arr.length; ++i) {
+        
+        int random = rand.nextInt(100);
+        arr[0+i] = random;
+        
+        if (arr[i] < mini) {
+            
+            mini = arr[i];
+            
+        }
+        
+        if (arr[i] > maxi) {
+            
+            maxi = arr[i];
+            
+        }
+        
+        } // end for
+        
+        System.out.println("Smallest: " + mini);
+        System.out.println("Largest: " + maxi);
     
-    /*Init. Phase*/
+        /*Termination Phase*/
+        // Print two results (smallest and largest)
     
-    // Import Random Number Class, called RandomGenerator
-    // Initialize Array with 20 elements
-    // Minimum Value
-    // Maximum Value
-    
-    /*Processing Phase*/
-    // For (int i = 0; i < Array.length; i++)
-        // Use Random Number Class to change each element to a random value
-        // between 0 and 100 (using int)
-        //If (Array[i] > Minimum Value)
-            // Minimum Value = Array[i]
-        // Else If (Array[i] < Maximum Value)
-            // Maximum Value = Array[i]
-    
-    /*Termination Phase*/
-    // Print two results (smallest and largest)
-    
-}
+    } // end main
+} // end class
